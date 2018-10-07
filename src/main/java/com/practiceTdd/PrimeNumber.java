84d6b50eb71f01d0d21bab28ab24b7c08aae4bb8
Visweswar1387 <gvisweswar0@gmail.com>
2018-10-07 11:09:26 +0530

    Remove magic numbers

--- src/main/java/com/practiceTdd/PrimeNumber.java
+++ src/main/java/com/practiceTdd/PrimeNumber.java
@@ -1,19 +1,23 @@
 package com.practiceTdd;
 
 public class PrimeNumber {
+
     private static final int ZERO = 0;
+    private static final int ONE = 1;
+    private static final int TWO = 2;
 
     public String checkPrimeNumber(int input) {
 
-        if(input>1) {
+        if(input>ONE) {
 
-            for (int number = 2; number <= input / 2; number++) {
+            for (int number = TWO; number <= input / TWO; number++) {
                 if (input % number == ZERO) {
                     return "Not Prime Number";
                 }
             }
             return "Prime Number";
-        } else {
+        }
+        else {
             return "Prime number should be greater than one";
         }
     }
