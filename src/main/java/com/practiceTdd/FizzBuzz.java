0b7df4ac1d4b4e5e519cf2e0d94a44201c451583
Visweswar1387 <gvisweswar0@gmail.com>
2018-10-07 11:16:49 +0530

    Refactor FizzBuzz code Remove print number in for loop and add number variable into for loop

--- src/main/java/com/practiceTdd/FizzBuzz.java
+++ src/main/java/com/practiceTdd/FizzBuzz.java
@@ -1,20 +1,22 @@
 package com.practiceTdd;
 
 public class FizzBuzz {
+
+
     public void print(int startNumber, int endNumber) {
 
-        for(int printNumber=startNumber;printNumber<=endNumber;printNumber++) {
-            if(checkMultipleThreeAndFive(printNumber)) {
+        for(int number=startNumber;number<=endNumber;number++) {
+            if(checkMultipleThreeAndFive(number)) {
                 System.out.println("FizzBuzz");
             }
-            else if(checkMultipleOfThree(printNumber)) {
+            else if(checkMultipleOfThree(number)) {
                 System.out.println("Fizz");
             }
-            else if(checkMultipleOfFive(printNumber)) {
+            else if(checkMultipleOfFive(number)) {
                 System.out.println("Buzz");
             }
             else {
-                System.out.println(printNumber);
+                System.out.println(number);
             }
         }
     }
